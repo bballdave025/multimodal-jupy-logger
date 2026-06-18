19851f9e2ee28e5b639b4477139dd03450530fac
David Black <22400988+bballdave025@users.noreply.github.com>
2026-06-17 21:21:09 -0400

    incremental capture and state notebook save

---
+++ docs/dev_notes/template_both_better.py
@@ -0,0 +1,56 @@
+##  Code cell N, ANYTHING NEW?
+##+      MAKE SURE TO MATCH THE FILENAME WITH TREE OUTPUT
+
+hashes="#"*63; dashes="-"*59; equalses="="*59; print_start="___Fil" + \
+"e contents for___\n   'jupy_log/"; count_start="___File line coun" + \
+"t for___\n   'jupy_log/"; full_stem_path="C:/David/my_repos_dwb/m" + \
+"ultimodal-jupy-logger/jupy_log/";      either_end=f"'___\n{dashes}\n";
+print(f"{hashes}"); print(); print(f"{dashes}\n");
+
+the_right_start=the_right_end="!";dpath_post_jupy_log="";art_str="";
+path_second_part="";path_third_part="";
+#a.1  #the_right_start = print_start;
+#a.2  #the_right_start = count_start;
+# b   #art_str="artifacts/";
+short_pth="";
+# c.1#dpath_post_jupy_log=f"{art_str}{short_pth}";
+path_second_part="";
+path_third_part ="";
+# c.2#dpath_post_jupy_log+=f"\n{path_second_part}\n{path_third_part}";
+path_pass=(  f"{full_stem_path}{art_str}{short_pth}{path_second_part}"
+f"{path_third_part}");print((f"{the_right_start}{dpath_post_jupy_log}"
+f"{either_end}") ); dpypd.print_file(path_pass) \
+if the_right_start == print_start  else dpypd.count_lines(path_pass); 
+contd_bigger_end=f"{dashes}\n{equalses}";
+contd_bigger_end=f"{dashes}\n{equalses}\n\n{equalses}\n{dashes}";
+#d.1  #the_right_end=contd_bigger_end;
+#d.2  #the_right_end=ended_bigger_end;
+print(the_right_end)
+
+##
+
+
+# hashes="#"*63; dashes="-"*59; equalses="="*59; print_start="___Fil" + \
+# "e contents for___\n   'jupy_log/"; count_start="___File line coun" + \
+# "t for___\n   'jupy_log/"; full_stem_path="C:/David/my_repos_dwb/m" + \
+# "ultimodal-jupy-logger/jupy_log/";either_end=f"'___\n{dashes}\n";print(
+# f"{hashes}\n);print();print(f"{dashes}\n");
+
+# the_right_start=the_right_end="!; "dpath_post_jupy_log="";  art_str="";
+# #a.1  #the_right_start = print_start;
+# #a.2  #the_right_start = count_start;
+# # b   #art_str="artifacts/";
+# short_pth="";
+# #  c.1#dpath_post_jupy_log=f"{art_str}{short_pth}\n";
+# path_second_part="";
+# path_third_part ="";
+# #  c.2#dpath_post_jupy_log=f"\n{path_second_part}\n{path_third_part}");
+# path_pass=f"{full_stem_path}{art_str}{short_pth}{dpath_post_jupy_log}";
+# print(  the_right_start   +   dpath_post_jupy_log    +   either_end  );
+# dpypd.print_lines(path_pass)  if the_right_start == print_start  else \
+# dpypd.count_lines(path_pass); contd_bigger_end=f"{dashes}\n{equalses}";
+# ended_bigger_end="f{dashes}\n\n\n{hashes}";
+# #d.1  #the_right_end=contd_bigger_end;
+# #d.2  #the_right_end=ended_bigger_end;
+
+# ##
